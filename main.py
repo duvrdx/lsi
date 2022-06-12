8fb3fa54cc351bad2b8fae309f86e3e00a272273
duvrdx <duvrdx@gmail.com>
2022-06-11 22:21:43 -0300

    adicionando readme em portugues

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ from datetime import datetime
 from app import app, db
 from app.models import Usuario, Produto, Agendamento
 
-db.create_all()
+# Criando uma webview onde será exibida e executada a aplicação web
 window = webview.create_window('Easy Software', app, width=1280, height=720, resizable=False, confirm_close=True)
 
+# Redirecionando o usuário para a tela de login
 @app.route('/')
 def index():
     return redirect(url_for('login'))
 
+# Modelagem da rota de login
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
@@ -24,6 +26,11 @@ def login():
         # Realizar uma query pra saver se existe um usuario
         user = Usuario.query.filter_by(username=username_input).first()
         
+        # Caso exista um usuário, é verificada a senha inserida para que o login seja 
+        # validado, caso o usuário não exista ou a senha inserida seja incorreta
+        # o usuário é redirecionado a tela de login, e em caso de sucesso
+        # o usuário é autenticado, e logado
+
         if not user:
             print('Usuário inexistente')
             return redirect(url_for('login'))
@@ -39,6 +46,7 @@ def login():
     
     return render_template('login.html')
 
+# Modelagem da rota de cadastro
 @app.route('/cadastro', methods=['GET','POST'])
 def cadastro():
     if request.method == 'POST':
@@ -50,6 +58,8 @@ def cadastro():
         # Realizar uma query pra saver se existe um usuario
         user = Usuario.query.filter_by(username=username_input).first()
         
+        # Caso não exista um usuário com o mesmo username, é criado um novo usuário e ele é redirecionado a tela de login
+        # caso contrário, o usuário é redirecionado a tela de cadastro
         if not user:
             new_user = Usuario(name_input, username_input, role_input, password_input);
             db.session.add(new_user)
@@ -62,6 +72,7 @@ def cadastro():
     
     return render_template('cadastro.html')
 
+# Modelagem da rota de logout
 @app.route('/logout')
 def logout():
     # Realizando query para buscar o usuário atual e realizando o logout
@@ -69,6 +80,7 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
+# Modelagem da rota da homepage
 @app.route('/homepage')
 def homepage():
     if current_user.is_authenticated:
@@ -77,6 +89,7 @@ def homepage():
         flash('Você precisa estar logado para acessar essa página.')
         return redirect(url_for('login'))
 
+# Modelagem da rota de estoque
 @app.route('/estoque', methods=['GET','POST'])
 def estoque():
     if request.method == 'POST':
@@ -92,11 +105,13 @@ def estoque():
         
     return render_template('estoque.html')
 
+# Modelagem da rota das informações sobre o estoque
 @app.route('/estoque/info')
 def estoque_info():
     products = Produto.query.all()
     return render_template('estoque_info.html', products=products)
 
+# Modelagem da rota para agendamento das entregas
 @app.route('/estoque/agend', methods=['GET','POST'])
 def estoque_agend():
     if request.method == 'POST':
@@ -111,6 +126,7 @@ def estoque_agend():
 
     return render_template('estoque_agend.html', agendamentos=agendamentos)
 
+# Modelagem da rota de produtos
 @app.route('/produtos', methods=['GET','POST'])
 def produtos():
     if request.method == 'POST':
@@ -128,10 +144,12 @@ def produtos():
     else:
         return render_template('produtos.html', disponibility=False, product="Pesquise por um produto.")
 
+# Modelagem da rota de estatisticas sobre os produtos
 @app.route('/produtos/estatisticas')
 def produtos_estatisticas():
     return render_template("estatisticas.html")
 
+# Modelagem da rota para remover um produto do estoque
 @app.route('/del/<barcode>')
 def delete(barcode):
     db.session.delete(Produto.query.filter_by(barcode=barcode).first())
@@ -139,12 +157,14 @@ def delete(barcode):
 
     return redirect(url_for("estoque_info"))
 
+# Modelagem da rota para mostrar prateleira
 @app.route('/prateleira')
 def prateleira():
     products = Produto.query.all()
     return render_template('prateleira.html', products=products)
 
 if __name__ == '__main__':
+    # Criando/Carregando banco de dados local e executando a aplicação
     db.create_all()
     webview.start()
    
---
+++ readme.md
@@ -0,0 +1,59 @@
+# **Gerenciador de Estoque | Inventory Manager**
+
+***PT*** <br>
+___________________________________________________
+
+## **Descrição**
+Este é um Aplicativo Web, desenvolvido para a disciplina de LSI (Laboratório de Sistemas de Informação). O aplicativo tem a proposta de ser um CRUD, voltado a um sistema de gestão de estoque, como: Entrada e Saída de Produtos, Sistema de Login e Cadastro, Cadastro de Produtos.
+
+O aplicativo foi desenvolvido usando a linguagem ***Python*** junto do framework ***Flask*** e algumas de suas bibliotecas, para transformar em um aplicativo desktop, foram usadas as bibliotecas ***pywebview*** e ***pyinstaller***, para rodar o aplicativo em um webview e criar um executável. 
+
+## **Instalação**
+
+Para executar em sua máquina, além da instalação do **Python**, é necessário instalar as bibliotecas utilizadas, que estão disponíveis em [requirements.txt](requirements.txt).
+
+### **Instalação usando pip**
+~~~
+pip install -r requirements.txt
+~~~
+
+### **Instalação usando conda**
+~~~
+conda install --file requirements.txt 
+~~~
+
+## **Execução**
+
+Para executar, basta rodar o arquivo [main.py](main.py)
+
+~~~
+python3 main.py
+~~~
+
+----------------------------------------------------
+<br>
+
+***EN*** <br>
+____________________________________________________
+
+## **Description**
+
+## **Installation**
+
+### **Install using pip**
+~~~
+pip install -r requirements.txt
+~~~
+
+### **Install using conda**
+~~~
+conda install --file requirements.txt 
+~~~
+
+## **Execution**
+
+To execute, run the file [main.py](main.py)
+
+~~~
+python3 main.py
+~~~
